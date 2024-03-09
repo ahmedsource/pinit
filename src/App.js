0980d96783e5d012f07b8b725c4523f24e5d8c80
ahmedsource <ahmedsource@gmail.com>
2024-03-09 19:19:16 +0200

    Added maps

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
+import React, {useContext, useEffect, useState} from 'react';
 import './App.css';
-
+import BingMapsReact from "bingmaps-react";
+import { FirebaseContext } from './utils/firebase';
+import { getFirestore, collection, getDocs, addDoc } from 'firebase/firestore/lite';
 function App() {
+  const firebase = useContext(FirebaseContext)
+  const db = getFirestore(firebase.app);
+  const [content, setContent]= useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [pins, setPins] = useState([]);
+  const [mapReady, setMapReady]=useState(false);
+
+  const getPosts = async()=>{
+    const ref = collection(db, "posts");
+    const snapshot = await getDocs(ref);
+    console.log({snapshot})
+    const pins = snapshot.docs.map(({_document:{data:{value:{mapValue: {fields}}}}})=>({
+      center: {
+        longitude: fields.longitude.doubleValue,
+        latitude: fields.latitude.doubleValue
+      },
+      options: {
+        color: 'purple',
+        description: fields.content.stringValue,
+      }
+    }))
+    setPins(pins)
+  }
+
+  useEffect(() => {
+    getPosts();
+    return () => {}
+  },[db])
+
+  const onSubmit = async (e)=>{
+    e.preventDefault();
+    setIsLoading(true)
+    if(!navigator.geolocation) alert("make sure your browser supports geolocation")
+    navigator.geolocation.getCurrentPosition(async({coords: {latitude, longitude}})=>{
+      if(!content || !content.length) alert('please Enter text');
+      const post = {
+        latitude,
+        longitude,
+        content,
+        date: new Date().toDateString(),
+        time: new Date().toTimeString()
+      };
+      const docRef = await addDoc(collection(db, "posts"), post);
+      console.log({docRef})
+      await getPosts()
+      setIsLoading(false)
+      setContent('')
+    })
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header-nav'>
+        <div className='logo-title'>
+          <div className='logo' />
+          <div>Pin It!</div>
+        </div>
+        <form name="content" onSubmit={e=>onSubmit(e)}>
+          <input type="text" value={content} onChange={e=>setContent(e.target.value)} placeholder="whats on your mind?" disabled={isLoading} />
+        </form>
+        {!!isLoading &&
+          <div className='loading'/>
+        }
+      </div>
+      {process.env.REACT_APP_BING_MAPS_KEY &&
+        <BingMapsReact 
+          bingMapsKey={process.env.REACT_APP_BING_MAPS_KEY}
+          height="100vh"
+          width="100%"
+            mapOptions={{
+              navigationBarMode: "square",
+          }}
+          onMapReady={()=>setMapReady(true)}
+          pushPinsWithInfoboxes={mapReady && pins}
+          />
+      }
     </div>
   );
 }
---
+++ src/utils/geolocation.js
@@ -0,0 +1,1 @@
+import {getCurrentPosition} from 'geolocation';
